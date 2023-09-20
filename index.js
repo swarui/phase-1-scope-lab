6b2aa6a2c40d97ad300d6c0f97f08f48135efe8a
stevewarui,@gmail.com <stevewarum@gmail.com>
2023-09-20 18:23:09 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName='bob';
+
+function Name(){
+    return customerName;
+}
+function upperCaseCustomerName(){
+    if(customerName){
+    customerName= customerName.toUpperCase();
+    }
+}
+var bestCustomer;
+
+function setBestCustomer(){
+    bestCustomer='not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    bestCustomer='maybe bob'
+    return bestCustomer;
+}
+const leastfavouritecustomer = "Steve"
+function changeLeastFavoriteCustomer(){
+    leastfavouritecustomer ='warui';
+}
